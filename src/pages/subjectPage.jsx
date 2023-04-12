bd2bd0e138730719f0eda85f548b13b3abb85d3f
AnkitVlekhak <ankitprime16@gmail.com>
2023-04-12 13:59:18 +0530

    dee

--- src/pages/subjectPage.jsx
+++ src/pages/subjectPage.jsx
@@ -2,10 +2,19 @@ import React, { useContext, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const subjectPage = () => {
-    // const [subject, setSubjects] = useState();
+    // // const [subject, setSubjects] = useState();
+    // const { subjects, setSubjects } = useContext(subjectsContext);
     // const { subjectName } = useParams();
-    // const notes = subject.subjectName;
-    // console.log(notes)
+    // // const notes = subject.subjectName;
+    // // console.log(notes)
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     // setSubjects({...subjects,})
+    //     e.target.topic
+    //     e.target.text
+    //     e.target.link
+    //     subjects.subjectName[];
+    // }
     return (
         <div>
 
